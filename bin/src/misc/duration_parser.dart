8de34c097ddaa9e987ed56165d0e901cbc695a72
Max Riegler <rinukkusu@sub-r.de>
2017-10-10 09:40:53 +0200

    Allow entry of seconds without "s" suffix

--- bin/src/misc/duration_parser.dart
+++ bin/src/misc/duration_parser.dart
@@ -2,7 +2,7 @@ part of irc_bot;
 
 Duration parseDuration(String durationString) {
   RegExp regex =
-      new RegExp(r"^(?:(\d+)d)?\s*(?:(\d+)h)?\s*(?:(\d+)m)?\s*(?:(\d+)s)?$");
+      new RegExp(r"^(?:(\d+)d)?\s*(?:(\d+)h)?\s*(?:(\d+)m)?\s*(?:(\d+)s?)?$");
   var match = regex.firstMatch(durationString);
   var days = int.parse(match?.group(1) ?? "0");
   var hours = int.parse(match?.group(2) ?? "0");
